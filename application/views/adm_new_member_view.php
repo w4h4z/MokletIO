8925468af21888d1334e677847cce66742be4ef2
wahaz <rizaldi.wahaz@gmail.com>
2017-12-07 14:03:28 +0700

    detail new member

--- application/views/adm_new_member_view.php
+++ application/views/adm_new_member_view.php
@@ -1,92 +1,88 @@
-  <section class="content-header">
-      <h1>
-        Manage New Register Member
-        <small><?php echo $sub->SINGKATAN_SUB ;?></small>
-      </h1>
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-      <div class="row">
-        <div class="col-xs-12">
-            <div class="box">
-            <div class="box-header">
-              <h3 class="box-title">Data New Register Member</h3>
-              <br>
-              <br>
-              <button class="btn btn-primary" data-toggle="modal" data-target="#modal_form"><span class="glyphicon glyphicon-plus"></span> Add New Member</button>
-<?php
-                  $failed = $this->session->flashdata('failed');
-                    if(!empty($failed)){
-                      echo '<div class="alert alert-danger alert-dismissable"  style="margin-top: 10px">';
-                      echo '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>';
-                      echo '<i class="icon fa fa-warning"></i>';
-                      echo $failed;
-                      echo '</div>';
-                    }
-
-                  $success = $this->session->flashdata('success');
-                  if(!empty($success)){
-                      echo '<div class="alert alert-success alert-dismissable"  style="margin-top: 10px">';
-                      echo '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>';
-                      echo '<i class="icon fa fa-check"></i>';
-                      echo $success;
-                      echo '</div>';
-                  }
+<section class="content-header">
+  <h1>
+    Manage New Register Member
+    <small><?php echo $sub->SINGKATAN_SUB ;?></small>
+  </h1>
+</section>
+<!-- Main content -->
+<section class="content">
+  <div class="row">
+  <div class="col-xs-12">
+    <div class="box">
+      <div class="box-header">
+        <h3 class="box-title">Data New Register Member</h3>
+        <br>
+        <br>
+        <button class="btn btn-primary" data-toggle="modal" data-target="#modal_form"><span class="glyphicon glyphicon-plus"></span> Add New Member</button>
+        <?php
+          $failed = $this->session->flashdata('failed');
+            if(!empty($failed)){
+              echo '<div class="alert alert-danger alert-dismissable"  style="margin-top: 10px">';
+              echo '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>';
+              echo '<i class="icon fa fa-warning"></i>';
+              echo $failed;
+              echo '</div>';
+            }
+          
+          $success = $this->session->flashdata('success');
+          if(!empty($success)){
+              echo '<div class="alert alert-success alert-dismissable"  style="margin-top: 10px">';
+              echo '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>';
+              echo '<i class="icon fa fa-check"></i>';
+              echo $success;
+              echo '</div>';
+          }
+          ?>
+        <!-- /.box-header -->
+        <div class="box-body table-responsive">
+          <table id="newMember" class="table table-bordered table-striped table-hover">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Name</th>
+                <th>Generation</th>
+                <th>Class</th>
+                <th>Phone Number</th>
+                <th>Email</th>
+                <th>Approval</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php 
+                $i = 1;
+                foreach ($member as $data) {
+                  echo '
+                      <tr>
+                                  <td>'.$i++.'</td>
+                                  <td>'.$data->NAMA_MEMBER.'</td>
+                                  <td>'.$data->ANGKATAN_MEMBER.'</td>
+                                  <td>'.$data->KELAS_MEMBER.'</td>
+                                  <td>'.$data->NO_HP_MEMBER.'</td>
+                                  <td>'.$data->EMAIL_MEMBER.'</td>
+                                  <td><button class="btn btn-success btn-sm"><span class="glyphicon glyphicon-check"></span> Approve</button></td>
+                                  <td><button class="btn btn-info btn-sm" data-toggle="modal" data-target="#modal_detail'.$data->ID_MEMBER.'"><span class="glyphicon glyphicon-zoom-in"></span></button>
+                                      <button class="btn btn-warning btn-sm"><span class="glyphicon glyphicon-pencil"></span></button>
+                                      <button class="btn btn-danger btn-sm"><span class="glyphicon glyphicon-trash"></span></button>
+                                  </td>
+                                </tr>
+                  '; 
+                }
+                  
                 ?>
-                
-            <!-- /.box-header -->
-            <div class="box-body table-responsive">
-              <table id="newMember" class="table table-bordered table-striped table-hover">
-                <thead>
-                <tr>
-                  <th>No</th>
-                  <th>Name</th>
-                  <th>Generation</th>
-                  <th>Class</th>
-                  <th>Phone Number</th>
-                  <th>Email</th>
-                  <th>Approval</th>
-                  <th>Action</th>
-                </tr>
-                </thead>
-                <tbody>
-<?php 
-$i = 1;
-foreach ($member as $data) {
-  echo '
-      <tr>
-                  <td>'.$i++.'</td>
-                  <td>'.$data->NAMA_MEMBER.'</td>
-                  <td>'.$data->ANGKATAN_MEMBER.'</td>
-                  <td>'.$data->KELAS_MEMBER.'</td>
-                  <td>'.$data->NO_HP_MEMBER.'</td>
-                  <td>'.$data->EMAIL_MEMBER.'</td>
-                  <td><button class="btn btn-success btn-sm"><span class="glyphicon glyphicon-check"></span> Approve</button></td>
-                  <td><button class="btn btn-info btn-sm" data-toggle="modal" data-target="#modal_detail'.$data->ID_MEMBER.'"><span class="glyphicon glyphicon-zoom-in"></span></button>
-                      <button class="btn btn-warning btn-sm"><span class="glyphicon glyphicon-pencil"></span></button>
-                      <button class="btn btn-danger btn-sm"><span class="glyphicon glyphicon-trash"></span></button>
-                  </td>
-                </tr>
-  '; 
-}
-  
-?>
-                </tbody>
-              </table>
-            </div>
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
-          </div>     
+            </tbody>
+          </table>
+        </div>
+        <!-- /.box-body -->
       </div>
-      <!-- /.row -->
-  </section>
-    <!-- /.content -->
-
-
-  <!-- Bootstrap modal -->
-  <div class="modal fade" id="modal_form" role="dialog">
+      <!-- /.box -->
+    </div>
+  </div>
+  <!-- /.row -->
+</section>
+<!-- /.content -->
+<!-- Bootstrap modal -->
+<div class="modal fade" id="modal_form" role="dialog">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
@@ -119,36 +115,36 @@ foreach ($member as $data) {
                   <option value="XRPL4">XRPL4</option>
                   <option value="XRPL5">XRPL5</option>
                   <option value="XRPL6">XRPL6</option>
-                  <option value="XTKJ1">XTKJ1</option>            
-                  <option value="XTKJ2">XTKJ2</option>            
-                  <option value="XTKJ3">XTKJ3</option>            
-                  <option value="XTKJ4">XTKJ4</option>            
-                  <option value="XTKJ5">XTKJ5</option>            
-                  <option value="XTKJ6">XTKJ6</option> 
+                  <option value="XTKJ1">XTKJ1</option>
+                  <option value="XTKJ2">XTKJ2</option>
+                  <option value="XTKJ3">XTKJ3</option>
+                  <option value="XTKJ4">XTKJ4</option>
+                  <option value="XTKJ5">XTKJ5</option>
+                  <option value="XTKJ6">XTKJ6</option>
                   <option value="XIRPL1">XIRPL1</option>
                   <option value="XIRPL2">XIRPL2</option>
                   <option value="XIRPL3">XIRPL3</option>
                   <option value="XIRPL4">XIRPL4</option>
                   <option value="XIRPL5">XIRPL5</option>
                   <option value="XIRPL6">XIRPL6</option>
-                  <option value="XITKJ1">XITKJ1</option>            
-                  <option value="XITKJ2">XITKJ2</option>            
-                  <option value="XITKJ3">XITKJ3</option>            
-                  <option value="XITKJ4">XITKJ4</option>            
-                  <option value="XITKJ5">XITKJ5</option>            
-                  <option value="XITKJ6">XITKJ6</option> 
+                  <option value="XITKJ1">XITKJ1</option>
+                  <option value="XITKJ2">XITKJ2</option>
+                  <option value="XITKJ3">XITKJ3</option>
+                  <option value="XITKJ4">XITKJ4</option>
+                  <option value="XITKJ5">XITKJ5</option>
+                  <option value="XITKJ6">XITKJ6</option>
                   <option value="XIIRPL1">XIIRPL1</option>
                   <option value="XIIRPL2">XIIRPL2</option>
                   <option value="XIIRPL3">XIIRPL3</option>
                   <option value="XIIRPL4">XIIRPL4</option>
                   <option value="XIIRPL5">XIIRPL5</option>
                   <option value="XIIRPL6">XIIRPL6</option>
-                  <option value="XIITKJ1">XIITKJ1</option>            
-                  <option value="XIITKJ2">XIITKJ2</option>            
-                  <option value="XIITKJ3">XIITKJ3</option>            
-                  <option value="XIITKJ4">XIITKJ4</option>            
-                  <option value="XIITKJ5">XIITKJ5</option>            
-                  <option value="XIITKJ6">XIITKJ6</option>            
+                  <option value="XIITKJ1">XIITKJ1</option>
+                  <option value="XIITKJ2">XIITKJ2</option>
+                  <option value="XIITKJ3">XIITKJ3</option>
+                  <option value="XIITKJ4">XIITKJ4</option>
+                  <option value="XIITKJ5">XIITKJ5</option>
+                  <option value="XIITKJ6">XIITKJ6</option>
                 </select>
               </div>
             </div>
@@ -183,60 +179,107 @@ foreach ($member as $data) {
               </div>
             </div>
           </div>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
-            <button type="submit" class="btn btn-primary">Save</button>
-          </form>
-          </div>
-        </div><!-- /.modal-content -->
-      </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
-  <!-- End Bootstrap modal -->
-
-
-<?php
-foreach ($member as $data) {
-  echo '
-    <!-- Bootstrap modal -->
-  <div class="modal fade" id="modal_detail'.$data->ID_MEMBER.'" role="dialog">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h3 class="modal-title">Detail New Member</h3>
       </div>
-      <div class="modal-body form">
-        <img src="'.base_url().'uploads/'.$data->FOTO_MEMBER.'" class="form-control col-lg-offset-3 col-xs-offset-3" style="height: 50%;width: 50%">
-        
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-          </div>
-        </div><!-- /.modal-content -->
-      </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
-  <!-- End Bootstrap modal -->
-  ';
-}
-
-?>
-
+      <div class="modal-footer">
+      <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+      <button type="submit" class="btn btn-primary">Save</button>
+      </form>
+      </div>
+    </div>
+    <!-- /.modal-content -->
+  </div>
+  <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
+<!-- End Bootstrap modal -->
+<?php
+  foreach ($member as $data) {
+    echo '
+      <!-- Bootstrap modal -->
+    <div class="modal fade" id="modal_detail'.$data->ID_MEMBER.'" role="dialog">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h3 class="modal-title">Detail New Member</h3>
+        </div>
+        <div class="modal-body form">
+          <img src="'.base_url().'uploads/'.$data->FOTO_MEMBER.'" class="form-control col-lg-offset-3 col-xs-offset-3" style="height: 50%;width: 50%; margin-bottom: 20px">
+            <div class="row">
+              <div class="form-group col-lg-offset-2 col-xs-offset-2">
+                <label class="control-label col-md-4 text-right">Name</label>
+                <div class="col-md-8">
+                  '.$data->NAMA_MEMBER.'
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="form-group col-lg-offset-2 col-xs-offset-2">
+                <label class="control-label col-md-4 text-right">Generation</label>
+                <div class="col-md-8">
+                  '.$data->ANGKATAN_MEMBER.'
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="form-group col-lg-offset-2 col-xs-offset-2">
+                <label class="control-label col-md-4 text-right">Class</label>
+                <div class="col-md-8">
+                  '.$data->KELAS_MEMBER.'
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="form-group col-lg-offset-2 col-xs-offset-2">
+                <label class="control-label col-md-4 text-right">Phone Number</label>
+                <div class="col-md-8">
+                  '.$data->NO_HP_MEMBER.'
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="form-group col-lg-offset-2 col-xs-offset-2">
+                <label class="control-label col-md-4 text-right">Email</label>
+                <div class="col-md-8">
+                  '.$data->EMAIL_MEMBER.'
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="form-group col-lg-offset-2 col-xs-offset-2">
+                <label class="control-label col-md-4 text-right">Reason to Join</label>
+                <div class="col-md-8">
+                  '.$data->ALASAN_MEMBER.'
+                </div>
+              </div>
+            </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+          </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+      </div><!-- /.modal -->
+    <!-- End Bootstrap modal -->
+    ';
+  }
+  
+  ?>
 <script>
   function readURL(input) {
     if (input.files && input.files[0]) {
       var reader = new FileReader();
-
+  
       reader.onload = function(e) {
         $('#preview').attr('src', e.target.result);
       }
-
+  
       reader.readAsDataURL(input.files[0]);
     }
   }
-
+  
   $("#imgInp").change(function() {
     readURL(this);
   });
-
+  
 </script>
