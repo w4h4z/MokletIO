3b3e77d93e4d52faae4373a2e52275284f0107c3
wahaz <rizaldi.wahaz@gmail.com>
2018-01-09 07:18:15 +0700

    Edit Event

--- application/models/event_model.php
+++ application/models/event_model.php
@@ -58,6 +58,102 @@ class Event_model extends CI_Model {
 		return true;	
 	}
 
+	public function updateEvent($idEvent,$banner,$poster)
+	{
+		$data1 = array('NAMA_EVENT'		=> $this->input->post('name'),
+					   'SUB_NAMA_EVENT'	=> $this->input->post('title')
+					);
+
+		$this->db->where('ID_EVENT', $idEvent)->update('event_sub', $data1);
+
+		$data2 = array('NAMA_DETAIL'		=> $this->input->post('detail'),
+					   'NAMA_MINI_TITLE'	=> $this->input->post('miniName'),
+					   'SUB_NAMA_TITLE'		=> $this->input->post('subName'),
+					   'DESC_DETAIL'		=> $this->input->post('desc'),
+					   'BANNER_DETAIL'		=> $banner,
+					   'POSTER_DETAIL'		=> $poster
+					);
+
+		$this->db->where('ID_EVENT', $idEvent)->update('detail_event', $data2);
+
+		if($this->db->affected_rows() == 0){
+			return false;
+		}
+
+		return true;	
+	}
+
+	public function updateEventBanner($idEvent,$banner)
+	{
+		$data1 = array('NAMA_EVENT'		=> $this->input->post('name'),
+					   'SUB_NAMA_EVENT'	=> $this->input->post('title')
+					);
+
+		$this->db->where('ID_EVENT', $idEvent)->update('event_sub', $data1);
+
+		$data2 = array('NAMA_DETAIL'		=> $this->input->post('detail'),
+					   'NAMA_MINI_TITLE'	=> $this->input->post('miniName'),
+					   'SUB_NAMA_TITLE'		=> $this->input->post('subName'),
+					   'DESC_DETAIL'		=> $this->input->post('desc'),
+					   'BANNER_DETAIL'		=> $banner
+					);
+
+		$this->db->where('ID_EVENT', $idEvent)->update('detail_event', $data2);
+
+		if($this->db->affected_rows() == 0){
+			return false;
+		}
+
+		return true;
+	}
+
+	public function updateEventPoster($idEvent,$poster)
+	{
+		$data1 = array('NAMA_EVENT'		=> $this->input->post('name'),
+					   'SUB_NAMA_EVENT'	=> $this->input->post('title')
+					);
+
+		$this->db->where('ID_EVENT', $idEvent)->update('event_sub', $data1);
+
+		$data2 = array('NAMA_DETAIL'		=> $this->input->post('detail'),
+					   'NAMA_MINI_TITLE'	=> $this->input->post('miniName'),
+					   'SUB_NAMA_TITLE'		=> $this->input->post('subName'),
+					   'DESC_DETAIL'		=> $this->input->post('desc'),
+					   'POSTER_DETAIL'		=> $poster
+					);
+
+		$this->db->where('ID_EVENT', $idEvent)->update('detail_event', $data2);
+
+		if($this->db->affected_rows() == 0){
+			return false;
+		}
+
+		return true;
+	}
+
+	public function updateEventWithoutPhoto($idEvent)
+	{
+		$data1 = array('NAMA_EVENT'		=> $this->input->post('name'),
+					   'SUB_NAMA_EVENT'	=> $this->input->post('title')
+					);
+
+		$this->db->where('ID_EVENT', $idEvent)->update('event_sub', $data1);
+
+		$data2 = array('NAMA_DETAIL'		=> $this->input->post('detail'),
+					   'NAMA_MINI_TITLE'	=> $this->input->post('miniName'),
+					   'SUB_NAMA_TITLE'		=> $this->input->post('subName'),
+					   'DESC_DETAIL'		=> $this->input->post('desc'),
+					);
+
+		$this->db->where('ID_EVENT', $idEvent)->update('detail_event', $data2);
+
+		if($this->db->affected_rows() == 0){
+			return false;
+		}
+
+		return true;
+	}
+
 }
 
 /* End of file event_model.php */
